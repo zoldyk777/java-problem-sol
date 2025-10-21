264f2844b16e4e4f01397baaf05373111813a1ed
zoldyk777 <adityabisht630@gmail.com>
2025-10-21 17:00:23 +0530

    sort

---
+++ insertionsort/insertionsort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class insertionsort {
+    public static void main(String[] args) {
+        int [] arr={3,5,2,6,1,8,7};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+    static void insertion(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    swap(arr,j,j-1);
+
+                } 
+                else{
+                    break;
+                }
+               
+            }
+
+        }
+
+    }
+    static void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+
+    }
+
+    
+    
+}
