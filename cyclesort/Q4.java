adaf8dfce135d59483610952cd574876151f5c2d
zoldyk777 <adityabisht630@gmail.com>
2025-10-25 19:36:32 +0530

    leetcode 442

---
+++ cyclesort/Q4.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Q4{
+    public List<Integer> findDuplicates(int[] nums) {
+        cy(nums);
+        List<Integer> an=new ArrayList();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i+1){
+                an.add(nums[i]);
+            }
+        }
+        return an;
+        
+    }
+    static void cy(int [] arr){
+        int i=0;
+        while(i<arr.length){
+            int c=arr[i]-1;
+            if(arr[i]!=arr[c]){
+                swap(arr,i,c);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    static void swap(int[] arr,int s,int e){
+        int temp=arr[s];
+        arr[s]=arr[e];
+        arr[e]=temp;
+    }
+}
