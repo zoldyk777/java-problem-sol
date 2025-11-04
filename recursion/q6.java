f60a5a722b520c9598e1809dff2a2f756561ab05
zoldyk777 <adityabisht630@gmail.com>
2025-11-04 21:23:09 +0530

    problem

---
+++ recursion/q6.java
@@ -0,0 +1,20 @@
+public class q6 {
+    public static void main(String[] args) {
+        tri(4, 0);
+        
+    }
+    static void tri(int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            System.out.print("*");
+            tri(r, c+1);
+        }
+        else{
+            System.out.println( );
+            tri(r-1, 0);
+        }
+    }
+    
+}
