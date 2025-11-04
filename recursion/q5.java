849eeb78dcc5e8ac29cf5377bf1681e82013d30c
zoldyk777 <adityabisht630@gmail.com>
2025-11-04 21:04:48 +0530

    problem

---
+++ recursion/q5.java
@@ -0,0 +1,30 @@
+public interface q5 {
+    public static void main(String[] args) {
+        int[] arr={5,6,7,8,9,1,2,3};
+        System.out.println(b(arr, 8, 0, arr.length-1));
+        
+    }
+    static int b(int[] arr,int tar,int s,int e){
+        if(s>e){
+            return -1;
+        }
+        int m=s+(e-s)/2;
+        if(arr[m]==tar){
+            return m;
+        }
+        if(arr[s]<=arr[m]){
+            if(tar>=arr[s] && tar<=arr[m]){
+                return b(arr, tar, s, m-1);
+            }
+            else{
+                return b(arr, tar, m+1, e);
+            }
+        }
+        if(tar>=arr[m] && tar<=arr[e]){
+            return b(arr, tar, m+1, e);
+        }
+        return b(arr, tar, s, m-1);
+
+         
+    }
+}
