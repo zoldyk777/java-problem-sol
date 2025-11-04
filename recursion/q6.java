a5ce40809ca0be793af75a4beedd3614af0de315
zoldyk777 <adityabisht630@gmail.com>
2025-11-04 21:28:17 +0530

    problem

--- recursion/q6.java
+++ recursion/q6.java
@@ -1,8 +1,10 @@
 public class q6 {
     public static void main(String[] args) {
+        tri2(4, 0);//reverse tri
         tri(4, 0);
         
     }
+    //reverse
     static void tri(int r,int c){
         if(r==0){
             return;
@@ -14,6 +16,20 @@ public class q6 {
         else{
             System.out.println( );
             tri(r-1, 0);
+        }
+    }
+    static void tri2(int r,int c){
+        if(r==0){
+            return;
+        }
+        if(c<r){
+            tri2(r, c+1);
+            System.out.print("*");
+            
+        }
+        else{
+            tri2(r-1, 0);
+            System.out.println();
         }
     }
     
