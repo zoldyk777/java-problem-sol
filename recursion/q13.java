70afa7ddc2cb8d57161ce34b5ff1e25d442eb909
zoldyk777 <adityabisht630@gmail.com>
2025-11-06 22:58:34 +0530

    problem

---
+++ recursion/q13.java
@@ -0,0 +1,17 @@
+public class q13 {
+    public static void main(String[] args) {
+        subseq("", "abc");
+        
+    }
+    // subsequence of string using recursion
+    static void subseq(String p,String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+        subseq(p,up.substring(1));
+        subseq(p+ch,up.substring(1));
+    }
+    
+}
