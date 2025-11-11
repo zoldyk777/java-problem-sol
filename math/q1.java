c76d5d5aac1b15cb8a0b54944d9e48305ab36621
zoldyk777 <adityabisht630@gmail.com>
2025-11-11 20:26:54 +0530

    problem

---
+++ math/q1.java
@@ -0,0 +1,23 @@
+class q1 {
+    public static void main(String[] args) {
+        int n=20;
+        for(int i=1;i<=n;i++){
+            System.out.println(i+"is prime"+isprime(i));
+        }
+        
+    }
+    
+    public static boolean isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        int c=2;
+        while(c*c<=n){
+            if(n%c==0){
+                return false;
+            }
+            c++;
+        }
+    return true;
+    }
+}
