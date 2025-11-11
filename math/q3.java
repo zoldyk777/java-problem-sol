6f8c3695d4035928f026a9af33d11c0b296a7b2c
zoldyk777 <adityabisht630@gmail.com>
2025-11-11 21:24:58 +0530

    problem

---
+++ math/q3.java
@@ -0,0 +1,36 @@
+
+public class q3 {
+    public static void main(String[] args) {
+        int n=40;
+        int p=3;
+        System.out.println(sqrt(n,p));
+        
+    }
+    static double sqrt(int n, int p){
+        int s=0;
+        int e=n;
+        double root=0.0;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(m*m==n){
+                return m;
+            }
+            if(m*m>n){
+                e=m-1;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        double inc=0.1;
+        for(int i=0;i<p;i++){
+            while(root*root<=n){
+                root+=inc;
+            }
+            root-=inc;
+            inc/=10;
+        }
+        return root;
+    }
+    
+}
