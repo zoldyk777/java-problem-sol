71a035f3be97fbb798b68a3970bee372fefbd380
zoldyk777 <adityabisht630@gmail.com>
2025-11-27 23:32:39 +0530

    coding question

---
+++ dailyleetcode/q17.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/maximum-subarray-sum-with-length-divisible-by-k/submissions/1841233466/?envType=daily-question&envId=2025-11-27
+class q17 {
+    public long maxSubarraySum(int[] nums, int k) {
+        long[] minPrefix = new long[k];
+        final long INF = Long.MAX_VALUE;
+
+        for (int i = 0; i < k; i++) minPrefix[i] = INF;
+        minPrefix[0] = 0;
+
+        long prefix = 0;
+        long answer = Long.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix += nums[i];
+            int mod = (i + 1) % k;
+
+            if (minPrefix[mod] != INF) {
+                answer = Math.max(answer, prefix - minPrefix[mod]);
+            }
+
+            minPrefix[mod] = Math.min(minPrefix[mod], prefix);
+        }
+
+        return answer;
+        
+        
+    }
+}
