78cbd318fff332eec0203748df328e8f6d22001a
zoldyk777 <adityabisht630@gmail.com>
2025-12-04 21:40:26 +0530

    coding question

---
+++ dailyleetcode/q24.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/count-collisions-on-a-road/description/?envType=daily-question&envId=2025-12-04
+class q24 {
+    public int countCollisions(String directions) {
+        int n = directions.length(), ans = 0, l = 0, r = n - 1; 
+        while(l < n && directions.charAt(l) == 'L') l++;
+        while(r >= l && directions.charAt(r) == 'R') r--;
+        for(int i = l ; i <= r; i++) {
+            if(directions.charAt(i) != 'S') ans++;
+        }
+        return ans;
+    }
+}
