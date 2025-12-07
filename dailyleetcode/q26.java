dad72ca351c8a54e634b9b06c6247970893394be
zoldyk777 <adityabisht630@gmail.com>
2025-12-07 21:24:40 +0530

    coding question

---
+++ dailyleetcode/q26.java
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/?envType=daily-question&envId=2025-12-04
+class q26 {
+    public int countOdds(int low, int high) {
+        return (high+1)/2-(low)/2;
+        
+    }
+}
