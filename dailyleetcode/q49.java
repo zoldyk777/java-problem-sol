c2d6e32b02ced2212c7edeffe3bbfaa8f99b29df
zoldyk777 <adityabisht630@gmail.com>
2026-05-04 22:36:12 +0530

    coding question

---
+++ dailyleetcode/q49.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/rotate-image/description/?envType=daily-question&envId=2026-05-04
+class q49 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int k = 0; k < n; k++) {
+            int i = 0, j = n - 1;
+            while (i < j) {  
+                int temp = matrix[k][i];
+                matrix[k][i] = matrix[k][j];
+                matrix[k][j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+}
