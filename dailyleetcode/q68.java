8af0923e3b3f2e8c8f7e3e0392b302868794190f
zoldyk777 <adityabisht630@gmail.com>
2026-07-19 00:13:59 +0530

    coding question

---
+++ dailyleetcode/q68.java
@@ -0,0 +1,24 @@
+https://leetcode.com/problems/find-greatest-common-divisor-of-array/description/?envType=daily-question&envId=2026-07-18
+class q68 {
+    public int findGCD(int[] nums) {
+        int mn = 1001, mx = 0;
+
+        for (int num : nums) {
+            if (num < mn)
+                mn = num;
+            if (num > mx)
+                mx = num;
+        }
+
+        return gcd(mn, mx);
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+        return a;
+    }
+}
