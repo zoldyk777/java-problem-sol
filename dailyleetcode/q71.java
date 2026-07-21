b2bfad01bde42136bb13d2158a1bd5fc8e5112fa
zoldyk777 <adityabisht630@gmail.com>
2026-07-21 23:07:07 +0530

    coding question

---
+++ dailyleetcode/q71.java
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/maximize-active-section-with-trade-i/description/?envType=daily-question&envId=2026-07-21a
+class q71 {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int ones = 0;
+        for (char c : s.toCharArray())
+            if (c == '1')
+                ones++;
+
+        s = "1" + s + "1";
+
+        int n = s.length();
+        int i = 0;
+
+        int ans = ones;
+
+        // Skip first 1's
+        while (i < n && s.charAt(i) == '1')
+            i++;
+
+        // Read first 0-block
+        int c10 = 0;
+        while (i < n && s.charAt(i) == '0') {
+            c10++;
+            i++;
+        }
+
+        while (i < n) {
+
+            // Read middle 1-block
+            int c11 = 0;
+            while (i < n && s.charAt(i) == '1') {
+                c11++;
+                i++;
+            }
+
+            if (c11 == 0)
+                break;
+
+            // Read right 0-block
+            int c20 = 0;
+            while (i < n && s.charAt(i) == '0') {
+                c20++;
+                i++;
+            }
+
+            if (c20 == 0)
+                break;
+
+            ans = Math.max(ans, ones + c10 + c20);
+
+            // Slide the window
+            c10 = c20;
+        }
+
+        return ans;
+    }
+}
