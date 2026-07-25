9aed0d1cca977dfaa4e28ff78558166b22043625
zoldyk777 <adityabisht630@gmail.com>
2026-07-25 21:35:47 +0530

    coding question

---
+++ dailyleetcode/q72.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-product-of-two-digits/description/?envType=daily-question&envId=2026-07-25
+class q72 {
+    public int maxProduct(int n) {
+        int max1 = -1 , max2 = -1;
+        while(n != 0){
+            int rem = n % 10;
+            if(max1 <= rem){
+                max2 = max1;
+                max1 = rem;
+            }
+            else if(max2 < rem){
+                max2 = rem;
+            }
+            n /= 10;
+        }
+        return max1 * max2;
+    }
+}
