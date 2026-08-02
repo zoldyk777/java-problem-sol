adc0f7b8fc5d9c7ed8e03697e9114f46c8a03f0b
zoldyk777 <adityabisht630@gmail.com>
2026-08-02 22:18:48 +0530

    daily question

---
+++ dailyleetcode/q82.java
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/stone-game/description/?envType=daily-question&envId=2026-08-02
+class q82 {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
