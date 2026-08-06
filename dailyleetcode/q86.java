bc40960ad8eeb5f2eafc8f18cd3c38830c1984e2
zoldyk777 <adityabisht630@gmail.com>
2026-08-06 23:40:20 +0530

    daily question

---
+++ dailyleetcode/q86.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/smallest-divisible-digit-product-i/description/?envType=daily-question&envId=2026-08-06
+class q86 {
+    public int smallestNumber(int n, int t) {
+        int q = n / 10, r = n % 10;
+
+        int req = t / gcd(q + (10 - q) / 10, t);
+        int nxt = ((r + req - 1) / req) * req;
+        int x = nxt - (nxt - 10) * (nxt / 10);
+
+        return q * 10 + x;
+    }
+
+    private int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+}
